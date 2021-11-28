a18f0150a4d59dbb0d4af6e44e4ebf731209ea55
=fuad <mfuad073@gmail.com>
2021-11-28 14:47:26 +0600

    updated java file

---
+++ keyboardwatcher/src/main/java/com/azimolabs/keyboardwatcher/NewFile.java
@@ -0,0 +1,15 @@
+package com.azimolabs.keyboardwatcher;
+
+import android.app.Activity;
+import android.os.Build;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.lang.ref.WeakReference;
+
+public class NewFile {
+
+
+}
--- keyboardwatcher/src/main/java/com/azimolabs/keyboardwatcher/newfile.java
+++
@@ -1,2 +0,0 @@
-hello this is a file
-i am a new ifle
